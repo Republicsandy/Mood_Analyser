0b2d86f0a0f02e8db9172e06d1ee3bb4505fdb2f
Sandeep singh <70006683+Republicsandy@users.noreply.github.com>
2022-09-07 07:49:23 +0530

    Update MoodAnalyser1.cs

--- Mood_Analyser/MoodAnalyser1.cs
+++ Mood_Analyser/MoodAnalyser1.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Mood_Analyser
 {
-    public class MoodAnalyser1
+    public class MoodAnalyzerFactory
     {
-        private string message;
-
-        public MoodAnalyser1(string message)
+        public static object CreateMoodAnalyserObject(string className, string constructorName)
         {
-            this.message = message;
-        }
-
-        public string AnalyseMood(string message)
-        {
-            throw new NotImplementedException();
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new CustomException(CustomException.ExceptionType.NO_SUCH_CLASS, "class not found.");
+                }
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionType.NO_SUCH_METHOD, "constructor not found.");
+            }
         }
     }
 }
