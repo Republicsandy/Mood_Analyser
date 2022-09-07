1a7d82b7793a175ac75e69b8817cef51953dcf57
Sandeep singh <sandy180388@gmail.com>
2022-09-07 07:22:27 +0530

    [sandeep] invaled input file is added in UC_3

--- Unit_Test_Mood/Unit_Test1.cs
+++ Unit_Test_Mood/Unit_Test1.cs
@@ -7,28 +7,62 @@ namespace Unit_Test_Mood
     [TestClass]
     public class UNit_Test1
     {
-        [Test]
+        [TestMethod]
         public void Given_I_Am_In_Sad_Mood_Should_Return_SAD()
         {
             //Arrange
             string message = "I am in sad mood.";
-            MoodAnalyser moodanalyser = new MoodAnalyser();
+            MoodAnalyser moodanalyser = new MoodAnalyser(message);
             //Act
             string result = moodanalyser.AnalyseMood(message);
             //Assert
             Assert.AreEqual("SAD", result);
         }
-        [Test]
+        [TestMethod]
         public void Given_I_Am_In_Happy_Mood_should_return_Happy()
         {
             //Arrange
-            string message = "I am in Happy Mood";
-            string expected = "HAPPY";
-            MoodAnalyser moodanalyser = new MoodAnalyser();
+            string message = "I am in happy mood.";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
             //Act
-            string result = moodanalyser.AnalyseMood(message);
+            string result = moodAnalyser.AnalyseMood(message);
             //Assert
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual("HAPPY", result);
+
+            [TestMethod]
+        public void Given_Null_Mood_Should_Throw_MoodAnalysisException_Indicating_Null_Mood()
+        {
+            try
+            {
+                //Arrange
+                string message = null;
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                //Act
+                string result = moodAnalyser.AnalyseMood(message);
+            }
+            catch (CustomException e)
+            {
+                //Assert
+                Assert.AreEqual("Mood should not be null.", e.Message);
+            }
         }
+        [TestMethod]
+        public void Given_Empty_Mood_Should_Throw_MoodAnalysisException_Indicating_Empty_Mood()
+        {
+            try
+            {
+                //Arrange
+                string message = "";
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                //Act
+                string result = moodAnalyser.AnalyseMood(message);
+            }
+            catch (CustomException e)
+            {
+                //Assert
+                Assert.AreEqual("Mood should not be empty.", e.Message);
+            }
+        }
+
     }
 }
